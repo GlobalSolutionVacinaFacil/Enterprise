4c3eb46b65a7352f7064034f14fef7dde474bd67
BrenoMMassa <brenommassa@gmail.com>
2023-11-23 20:53:08 -0300

    Finalizado Crud Cliente

--- Fiap.Vacina.Facil/Controllers/ClienteController.cs
+++ Fiap.Vacina.Facil/Controllers/ClienteController.cs
@@ -33,20 +33,78 @@ namespace Fiap.Vacina.Facil.Controllers
         {
             var clientes = _context.Clientes
                 .Where(p => p.Nome.Contains(filtro) || string.IsNullOrEmpty(filtro))
+                .Where(p => p.SituacaoCliente == SituacaoCliente.ATIVO)
                 .Include(p => p.Endereco)
                 .ToList();
             return View(clientes);
         }
 
-        /*
-        public IActionResult Index(string filtro = "")
+        [HttpPost]
+        public IActionResult Deletar(int id)
         {
-            var lista = _context.Clientes
-                .Where(p => p.Nome.Contains(filtro) || string.IsNullOrEmpty(filtro))
-                .Include(p => p.Endereco)
-                .ToList();
-            return View(filtro);
-        }*/
+            try
+            {
+                var cliente = _context.Clientes.Where(c => c.ClienteId == id).FirstOrDefault();
 
+                if (cliente != null)
+                {
+                    cliente.SituacaoCliente = SituacaoCliente.INATIVO;
+                    _context.Update(cliente);
+                    _context.SaveChanges();
+                    TempData["msg"] = "Cliente deletado com sucesso!";
+                }
+                else
+                {
+                    TempData["msg"] = "Cliente não encontrado!";
+                }
+            }
+            catch (Exception)
+            {
+                TempData["msg"] = "Erro ao deletar o cliente!";
+            }
+
+            return RedirectToAction("Index");
+
+        }
+
+        [HttpGet]
+        public IActionResult Editar(int id)
+        {
+            try
+            {
+                var cliente = _context.Clientes.Where(c => c.ClienteId == id).FirstOrDefault();
+                cliente.Endereco = _context.Enderecos.Where(e => e.EnderecoId == cliente.EnderecoId).FirstOrDefault();
+
+                if(cliente == null)
+                {
+                    TempData["msg"] = "Cliente não encontrado!";
+                    return RedirectToAction("Index");
+                }
+
+                return View(cliente);
+            }
+            catch (Exception)
+            {
+                TempData["msg"] = "Erro ao editar cliente!";
+                return RedirectToAction("Index");
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Editar(Cliente cliente)
+        {
+            try
+            {
+                _context.Clientes.Update(cliente);
+                _context.SaveChanges();
+                TempData["msg"] = "Cliente editado";
+                return RedirectToAction("Index");
+            }
+            catch (Exception)
+            {
+                TempData["msg"] = "Erro ao editar cliente!";
+                return View(cliente);
+            }
+        }
     }
 }
